45a35a2e77de86e7eb5951cd412336874585eed5
Reece Ward <getreecenward@gmail.com>
2023-10-08 23:55:51 -0700

    Configured for production

--- server.js
+++ server.js
@@ -1,91 +1,98 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client'); // Import Prisma Client
-const cors = require('cors'); // Import the cors package
+const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
+const helmet = require('helmet'); // Add helmet for security headers
+const winston = require('winston'); // Add Winston for logging
 
 const app = express();
-const port = 3000; // You can change the port as needed
-const prisma = new PrismaClient(); // Initialize Prisma Client
+const port = process.env.PORT || 3000; // Use PORT environment variable for production
 
-// Define routes or middleware (e.g., for parsing JSON)
-app.use(express.urlencoded({ extended: true })); // To parse form data
+// Set up environment variables using dotenv for development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const prisma = new PrismaClient();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(helmet()); // Use Helmet for security headers
 
+// Custom error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+// Logging configuration
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+  ],
+});
+
+// Routes
 app.get('/', async (req, res) => {
-    try {
-      // Use Prisma Client to fetch all names from the "Test" model
-      const names = await prisma.test.findMany();
-  
-      // Send the names data as JSON response
-      res.json({ names });
-    } catch (error) {
-      console.error('Error fetching names:', error);
-      res.status(500).json({ error: 'Error fetching names' });
-    }
-  });
+  try {
+    const names = await prisma.test.findMany();
+    res.json({ names });
+  } catch (error) {
+    logger.error('Error fetching names:', error); // Log errors
+    res.status(500).json({ error: 'Error fetching names' });
+  }
+});
 
 app.post('/add-name', async (req, res) => {
-    try {
-      const { namex } = req.body; // Updated field name to "namex"
-  
-      // Use Prisma Client to create a new entry in the "Test" model
-      const newTestEntry = await prisma.test.create({
-        data: {
-          name: namex, // Updated field name to "namex"
-        },
-      });
-  
-      // Redirect to the home page to display the names
-      res.redirect('/');
-    } catch (error) {
-      console.error('Error adding name:', error);
-      res.status(500).send('Error adding name');
-    }
-  });
+  try {
+    const { namex } = req.body;
+    const newTestEntry = await prisma.test.create({
+      data: {
+        name: namex,
+      },
+    });
+    res.redirect('/');
+  } catch (error) {
+    logger.error('Error adding name:', error); // Log errors
+    res.status(500).send('Error adding name');
+  }
+});
 
-  app.put('/update-name/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { namex } = req.body; // Updated field name to "namex"
+app.put('/update-name/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { namex } = req.body;
+    const updatedTestEntry = await prisma.test.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name: namex,
+      },
+    });
+    res.json({ updatedTestEntry });
+  } catch (error) {
+    logger.error('Error updating name:', error); // Log errors
+    res.status(500).send('Error updating name');
+  }
+});
 
-      // Use Prisma Client to update the name in the "Test" model
-      const updatedTestEntry = await prisma.test.update({
-        where: {
-          id: parseInt(id),
-        },
-        data: {
-          name: namex, // Updated field name to "namex"
-        },
-      });
-
-      // Send a JSON response with the updated entry
-      res.json({ updatedTestEntry });
-    } catch (error) {
-      console.error('Error updating name:', error);
-      res.status(500).send('Error updating name');
-    }
-  });
-
-
-  app.delete('/delete-name/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-
-      // Use Prisma Client to delete the entry from the "Test" model
-      await prisma.test.delete({
-        where: {
-          id: parseInt(id),
-        },
-      });
-
-      // Send a success response
-      res.status(204).send();
-    } catch (error) {
-      console.error('Error deleting name:', error);
-      res.status(500).send('Error deleting name');
-    }
-  });
-
+app.delete('/delete-name/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await prisma.test.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.status(204).send();
+  } catch (error) {
+    logger.error('Error deleting name:', error); // Log errors
+    res.status(500).send('Error deleting name');
+  }
+});
 
 // Start the server
 app.listen(port, () => {
